64d642fd1876ac6c1905071aa70e9ac060a28ddb
biglate09 <biglate09@gmail.com>
2021-02-06 13:28:33 +0700

    fix

--- themes/twentytwenty/page-interviews.php
+++ themes/twentytwenty/page-interviews.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+require_once('custom-classes/class-posts.php');
+$interviewPostsObject = Post::getPostsByCategory('interviews', null, 9, 0, null);
+$interviewPosts = $interviewPostsObject->posts;
+$firstPost = $interviewPosts[0];
+array_shift($interviewPosts);
+//Shift out for the first post
+?>
 
 <head>
   <meta charset="UTF-8">
@@ -7,101 +15,79 @@
   <title>Interview</title>
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   <link href="https://cdn.lazywasabi.net/fonts/NotoSansThai/NotoSansThai.css" rel="stylesheet">
+  <style>
+    .interview-card {
+      height: 80vh;
+    }
+
+    .interview-cover {
+      background: url('<?= $firstPost->featuredImage ?>') no-repeat;
+      background-size: auto 100%;
+      background-position: top right;
+      height: 80vh
+    }
+
+    @media (min-width: 1024px) {
+      .interview-cover {
+        background-size: 100% auto;
+        background-position: top right;
+        height: 80vh
+      }
+
+      .interview-card {
+        height: 60vh;
+      }
+    }
+  </style>
 </head>
-<?php
-require_once('custom-classes/class-posts.php');
-$recentPosts = Post::getPostsByCategory('post', null, 12, 0, null);
-$interviewPosts = Post::getPostsByCategory('interviews', null, 8, 0, null);
-?>
 
 <body style="font-family: 'Noto Sans Thai', sans-serif;background-color: #19181F;" class="text-white w-full">
   <?php include 'truefriend-header.php'; ?>
   <!-- Set up your HTML -->
-  <section class="w-full pt-16">
-    <div class="interview-cover">
-      <div class="absolute w-full lg:pl-32 lg:pr-0 px-8 lg:w-1/2" style="z-index: 1;">
-        <div class="mb-16">
-          <p class="text-xl">สัมภาษณ์</p>
-          <p class="text-5xl font-bold mt-2">Interview</p>
-        </div>
-        <div>
-          <p class="border-b border-white pb-4 text-xl font-light w-2/4 md:w-full">ล่าสุด</p>
-          <div class="flex items-center py-4">
-            <img src="<?= get_theme_file_uri() ?>/assets/images/image 8.png" alt="" class="object-cover w-full h-full rounded-full" style="width:45px;height:45px;">
-            <p class="ml-4">Potato corner</p>
+  <?php if ($firstPost) { ?>
+    <section class="w-full pt-16">
+      <div class="relative interview-cover cursor-pointer" onclick="window.open('<?= $firstPost->link ?>','_self')">
+        <div class="absolute bottom-0 w-full lg:px-40 px-8 lg:w-1/2 lg:pb-16">
+          <div class="mb-16">
+            <p class="text-xl">สัมภาษณ์</p>
+            <p class="text-5xl font-bold mt-2">Interview</p>
           </div>
-          <p class="text-lg md:text-2xl break-words">ความสำเร็จของ "Potato Corner" จนต่อคิวซื้อเฟรนช์ฟรายส์</p>
+          <div>
+            <p class="border-b border-white pb-4 text-xl font-light">ล่าสุด</p>
+            <div class="flex items-center py-4">
+              <img src="<?= $firstPost->intervieweeBusinessLogo ?>" alt="" class="w-10 h-10 rounded-full object-cover">
+              <p class="ml-2"><?= $firstPost->intervieweeBusiness ? $firstPost->intervieweeBusiness : $firstPost->interviewee ?></p>
+            </div>
+            <p class="text-2xl"><?= $firstPost->title ?></p>
+          </div>
         </div>
       </div>
-      <div class="absolute top-0 h-full w-full">
-        <div class="h-1/2 absolute bottom-0 w-full" style="background-image: linear-gradient(180deg, rgba(0,0,0,0), rgba(25,25,32,1));"></div>
-        <img src="<?= get_theme_file_uri() ?>/assets/images/interview-page-cover-pc.png" class="interview-cover-img xs:none md:block" style="z-index: -1;">
-        <img src="<?= get_theme_file_uri() ?>/assets/images/interview-page-cover-mobile.png" class="interview-cover-img xs:block md:none" style="z-index: -1;">
-      </div>
-    </div>
-  </section>
-  <section class="grid lg:grid-cols-4 grid-cols-1 lg:px-5 px-4 gap-5 pb-12 -mt-6 md:-mt-12">
-    <?php foreach ($interviewPosts->posts as $thePost) :?>
-      <div class="relative cursor-pointer" onclick="window.open('<?= $thePost->link ?>','_blank')">
+    </section>
+  <?php } ?>
+  <section class="grid lg:grid-cols-4 grid-cols-1 lg:px-5 px-4 gap-5 pb-12 pt-16">
+    <?php
+    foreach ($interviewPosts as $item) {
+    ?>
+      <div class="relative cursor-pointer" onclick="window.open('<?= $item->link ?>','_self')">
         <div class="interview-card">
-          <img class="object-cover w-full h-full rounded-xl" src="<?= $thePost->featuredImage ?>" />
+          <img class="object-cover w-full h-full rounded-xl" src="<?= $item->featuredImage ?>" />
         </div>
-        <?php if ($thePost->intervieweeBusinessLogo) : ?>
         <div class="absolute top-3 left-3" style="width:45px;height:45px;">
-          <img class="object-cover w-full h-full rounded-full" src="<?= $thePost->intervieweeBusinessLogo ?>" />
+          <img class="object-cover w-full h-full rounded-full" src="<?= $item->intervieweeBusinessLogo ?>" />
         </div>
-        <?php endif; ?>
         <div class="absolute left-0 bottom-0" style="background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.6));">
           <div class="ml-6 mb-6 mr-6">
-            <p class="font-bold text-white text-base mb-2"><?= $thePost->intervieweeBusiness ? $thePost->intervieweeBusiness : $thePost->interviewee ?></p>
-            <p class="text-white text-base"><?= $thePost->title ?></p>
+            <p class="font-bold text-white text-base mb-2"><?= $item->intervieweeBusiness ? $item->intervieweeBusiness : $item->interviewee ?></p>
+            <p class="text-white text-base"><?= $item->title ?></p>
           </div>
         </div>
       </div>
-    <?php endforeach; ?>
-
-
+    <?php } ?>
   </section>
   <?php include 'truefriend-footer.php'; ?>
 </body>
 
 </html>
-
-<style>
-  .interview-card {
-    height: 80vh;
-  }
-
-  .interview-cover {
-    height: 85vh;
-    position: relative;
-    display: flex;
-    align-items: center;
-  }
-  .interview-cover-img{
-    position: absolute;
-    right: 10%;
-  }
-
-  @media (min-width: 1024px) {
-
-    .interview-card {
-      height: 60vh;
-    }
-  }
-  @media (max-width: 992px) {
-    .interview-cover {
-      display: block;
-      padding-top: 50px;
-      height: unset;
-      min-height: 60vh;
-    }
-    .interview-cover-img{
-      right: 0;
-      bottom: 0;
-    }
-  }
-</style>
 
 <!-- <script src="http://code.jquery.com/jquery-latest.min.js"></script> -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
