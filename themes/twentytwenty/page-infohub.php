bfb8ea43dd1651e11f9d4f3b909d10e4fd70f295
biglate09 <biglate09@gmail.com>
2021-02-10 23:35:00 +0700

    change color and logo of footer

--- themes/twentytwenty/page-infohub.php
+++ themes/twentytwenty/page-infohub.php
@@ -28,7 +28,7 @@ $franchise = Post::getPostsByCategory('franchises', null, 1, 0, null);
       <a href=""><img class="w-6 h-6 cursor-pointer" src="<?= get_theme_file_uri() ?>/assets/images/link-black-icon.png" alt="" /></a>
     </div>
     <div class="flex justify-center flex-wrap w-full mt-16 mb-12">
-      <a href="<?=get_site_url()?>/suppliers" class="lg:w-1/5 w-full flex lg:items-center lg:justify-center flex-col lg:h-72 h-40 relative lg:mb-0 mb-4 lg:ml-2 lg:mr-2 ml-4 mr-4 rounded-md" style="background-color:#FFD950">
+      <a href="<?= get_site_url() ?>/suppliers" class="lg:w-1/5 w-full flex lg:items-center lg:justify-center flex-col lg:h-72 h-40 relative lg:mb-0 mb-4 lg:ml-2 lg:mr-2 ml-4 mr-4 rounded-md" style="background-color:#FFD950">
         <div class="lg:text-center px-4 pt-4 lg:pb-10">
           <h1 class="text-3xl font-black tracking-tighter lg:mb-3 mb-2">Supplier</h1>
           <h2 class="text-xs lg:text-sm lg:text-center">รวมข้อมูล Supplier ทั่วประเทศไทย <br class="hidden lg:block" />กว่า 600 บริษัทในประเทศไทย</h2>
@@ -38,7 +38,7 @@ $franchise = Post::getPostsByCategory('franchises', null, 1, 0, null);
           <div class="flex items-center"><?= $supplier->posts_count ?>+ <img class="w-6 h-6" src="<?= get_theme_file_uri() ?>/assets/images/carbon-chevron-right-blue.svg" alt="" /></div>
         </div>
       </a>
-      <a href="<?=get_site_url()?>/restaurants" class="lg:w-1/5 w-full flex lg:items-center lg:justify-center flex-col lg:h-72 h-40 relative lg:mb-0 mb-4 text-white lg:ml-2 lg:mr-2 ml-4 mr-4 rounded-md" style="background-color:#262145">
+      <a href="<?= get_site_url() ?>/restaurants" class="lg:w-1/5 w-full flex lg:items-center lg:justify-center flex-col lg:h-72 h-40 relative lg:mb-0 mb-4 text-white lg:ml-2 lg:mr-2 ml-4 mr-4 rounded-md" style="background-color:#262145">
         <div class="lg:text-center px-4 pt-4 lg:pb-10">
           <h1 class="text-3xl font-black tracking-tighter lg:mb-3 mb-2">Restaurant</h1>
           <h2 class="text-xs lg:text-sm lg:text-center">รวมข้อมูลร้านอาหารต่างๆ<br class="hidden lg:block" />กว่า 600 ธุรกิจในประเทศไทย</h2>
@@ -48,7 +48,7 @@ $franchise = Post::getPostsByCategory('franchises', null, 1, 0, null);
           <div class="flex items-center"><?= $restaurant->posts_count ?>+ <img class="w-6 h-6" src="<?= get_theme_file_uri() ?>/assets/images/carbon-chevron-right.svg" alt="" /></div>
         </div>
       </a>
-      <a href="<?=get_site_url()?>/franchises" class="lg:w-1/5 w-full flex lg:items-center lg:justify-center flex-col lg:h-72 h-40 relative lg:mb-0 mb-4 bg-white lg:ml-2 lg:mr-2 ml-4 mr-4 rounded-md" style="color:#251D5C">
+      <a href="<?= get_site_url() ?>/franchises" class="lg:w-1/5 w-full flex lg:items-center lg:justify-center flex-col lg:h-72 h-40 relative lg:mb-0 mb-4 bg-white lg:ml-2 lg:mr-2 ml-4 mr-4 rounded-md" style="color:#251D5C">
         <div class="lg:text-center px-4 pt-4 lg:pb-10">
           <h1 class="text-3xl font-black tracking-tighter lg:mb-3 mb-2">Franchise</h1>
           <h2 class="text-xs lg:text-sm lg:text-center">ศูนย์รวมข้อมูลเฟรนชายกว่า<br class="hidden lg:block" />600 เฟรนชายในประเทศไทย</h2>
@@ -65,6 +65,12 @@ $franchise = Post::getPostsByCategory('franchises', null, 1, 0, null);
       <a href=""><img class="w-6 h-6 cursor-pointer" src="<?= get_theme_file_uri() ?>/assets/images/link-black-icon.png" alt="" /></a>
     </div>
   </section>
+  <?php
+  $footerbgcolor = '#f2f2f2';
+  $footercolor = '#19181F';
+  $footerheadercolor = 'rgba(0,0,0,0.5)';
+  $footerlogo = get_theme_file_uri() . '/assets/images/logo-blue.svg';
+  ?>
   <?php include 'truefriend-footer.php'; ?>
 </body>
 
--- themes/twentytwenty/truefriend-footer.php
+++ themes/twentytwenty/truefriend-footer.php
@@ -1,8 +1,8 @@
-<div class="flex flex-wrap justify-center text-white lg:pb-32 pb-16" style="background-color:#262145;">
+<div class="flex flex-wrap justify-center text-white lg:pb-32 pb-16" style="color:<?= $footercolor ? $footercolor : 'white' ?>;background-color:<?= $footerbgcolor ? $footerbgcolor : '#262145' ?>;">
 	<div class="flex justify-between w-full">
 		<div class="ml-8 mt-10 mb-10 lg:block hidden">
 			<div class="flex items-center justify-start mb-6">
-				<img src="<?= get_theme_file_uri() ?>/assets/images/logo-white.svg" class="cursor-pointer w-32" />
+				<img src="<?= $footerlogo ? $footerlogo : get_theme_file_uri() . '/assets/images/logo-white.svg' ?>" class="cursor-pointer w-32" />
 				<!-- <div class="text-sm leading-tight font-bold">
 					<p>เพื่อนแท้</p>
 					<p>ร้านอาหาร</p>
@@ -12,9 +12,9 @@
 			<p class="text-base"><a href="tel:0917804724">091 780 4724</a></p>
 		</div>
 
-		<div class="flex justify-between lg:w-1/4 w-full mr-8 ml-8 lg:ml-0 mt-14 lg:mt-10 text-xs" >
+		<div class="flex justify-between lg:w-1/4 w-full mr-8 ml-8 lg:ml-0 mt-14 lg:mt-10 text-xs">
 			<ul class="lg:mr-6">
-				<li class="mb-4"><a href="#" style="color:rgba(255,255,255,0.5)">Article</a></li>
+				<li class="mb-4"><a href="#" style="color:<?= $footerheadercolor ? $footerheadercolor : 'rgba(255,255,255,0.5)' ?>">Article</a></li>
 				<li class="mb-1"><a href="#">Restaurant 101</a></li>
 				<li class="mb-1"><a href="#">Knowledge</a></li>
 				<li class="mb-1"><a href="#">News</a></li>
@@ -22,13 +22,13 @@
 				<li class="mb-1"><a href="#">File</a></li>
 			</ul>
 			<ul class="lg:ml-6 lg:mr-6">
-				<li class="mb-4"><a href="#" style="color:rgba(255,255,255,0.5)" class="mb-4">Interview & Review</a></li>
+				<li class="mb-4"><a href="#" style="color:<?= $footerheadercolor ? $footerheadercolor : 'rgba(255,255,255,0.5)' ?>" class="mb-4">Interview & Review</a></li>
 				<li class="mb-1"><a href="#">Brand Story</a></li>
 				<li class="mb-1"><a href="#">Review</a></li>
 				<li class="mb-1"><a href="#">Franchise</a></li>
 			</ul>
 			<ul class="lg:ml-6">
-				<li class="mb-4"><a href="#" style="color:rgba(255,255,255,0.5)" class="mb-4">SupplierHub</a></li>
+				<li class="mb-4"><a href="#" style="color:<?= $footerheadercolor ? $footerheadercolor : 'rgba(255,255,255,0.5)' ?>" class="mb-4">SupplierHub</a></li>
 				<li class="mb-1"><a href="#">Profile List</a></li>
 				<li class="mb-1"><a href="#">Product</a></li>
 			</ul>
@@ -36,7 +36,7 @@
 	</div>
 	<div class="mt-10 lg:hidden text-center">
 		<div class="flex items-center justify-center mb-4">
-			<img src="<?= get_theme_file_uri() ?>/assets/images/logo-white.svg" class="cursor-pointer mr-2" />
+			<img src="<?= $footerlogo ? $footerlogo : get_theme_file_uri() . '/assets/images/logo-white.svg' ?>" class="cursor-pointer mr-2" />
 			<!-- <div class="text-base leading-tight font-bold">
 				<p>เพื่อนแท้</p>
 				<p>ร้านอาหาร</p>
